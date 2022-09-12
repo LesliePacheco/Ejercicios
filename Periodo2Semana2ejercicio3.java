929db91f2910cf41ebbea20d7be0e3de00d53ed4
LesliePacheco <113389987+LesliePacheco@users.noreply.github.com>
2022-09-12 17:02:40 -0600

    Add files via upload

---
+++ Periodo2Semana2ejercicio3.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class Main { 
+  
+  public static void main(String[] args) {
+  Scanner Entrada= new Scanner(System.in);
+   String str = "dos";
+    switch(str)
+   {    
+      case "uno":
+        System.out.println("uno");
+       break;
+       case "dos":
+        System.out.println("dos");
+       break;
+       case "tres":
+        System.out.println("tres");
+       break;
+       default:
+       System.out.println("no coincide");
+  }
+}
+   }
